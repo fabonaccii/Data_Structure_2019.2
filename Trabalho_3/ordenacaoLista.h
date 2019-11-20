b2a6cbd69b81c4107eb18c99ba1293566277f698
Fábio Luz <48682990+fluzufc@users.noreply.github.com>
2019-11-20 11:09:59 -0300

    Update (Functions insert)

--- Trabalho_3/ordenacaoLista.h
+++ Trabalho_3/ordenacaoLista.h
@@ -1,16 +1,67 @@
 #ifndef ORDENACAO_LISTA_H
 #define ORDENACAO_LISTA_H
 
-void listBubbleSort ();
+struct Node;
 
-void listInsertionSort ();
+class List {
+    private:
+        //Cabeçalho da lista
+        Node *head;
 
-void listSelectionSort ();
+    public:
+        //Construtor
+        List();
 
-void listMergeSort ();
+        //Destrutor
+        ~List();
 
-void listHeapSort ();
+        //Insere um inteiro key ao final da lista
+        void pushBack (int key);
 
-void listQuickSort ();
+        //Remove elemento do final da lista e retorna seu valor
+        int popBack();
+
+        //Insere um novo nó com valor key após o index-ésimo nó da lista
+        void insertAfter (int key, int index);
+
+        //Busca um valor e retorna um ponteiro
+        Node *search (int key);
+
+        //Imprime os elementos da lista
+        void print();
+
+        //Retorna true se a lista estiver vazia ou false, caso contrário
+        bool isEmpty();
+
+        //Retorna o número de nós da lista
+        int size();
+
+        //Remove todos os elementos da lista e deixa apenas o nó cabeça
+        void clear();
+
+        //Retorna um ponteiro para uma cópia desta lista
+        List *copy();
+
+        //Copia os elementos do vetor array para a lista
+        void copyArray (int *array, int array_size);
+
+        //Bubble Sort
+        void bubbleSort (List *list);
+
+        //Insertion Sort
+        void insertionSort (List *list);
+
+        //Selection Sort
+        void selectionSort (List *list);
+
+        //Merge Sort
+        void mergeSort (List *list);
+
+        //Heap Sort
+        void heapSort (List *list);
+
+        //Quick Sort
+        void quickSort (List *list);
+};
 
 #endif
