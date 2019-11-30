29890401bd83534399d4afa4d891359ce3bb6608
Fábio Luz <fabioluzduartefilho@alu.ufc.br>
2019-11-30 18:52:31 -0300

    Update ordenacaoVetor.cpp

--- Trabalho_3/ordenacaoVetor.cpp
+++ Trabalho_3/ordenacaoVetor.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 #include "ordenacaoVetor.h"
 
-
 void swap (int* x, int* y) {
     int z = *x;
     *x = *y;
     *y = z;
 }
 
-
-
-void iteractiveBubbleSort (int *array, int size) {
+void iterativeBubbleSort (int *array, int size) {
     bool swapped;
     for (int i = 0; i < size-1; i++) {
         swapped = false;
@@ -34,13 +31,13 @@ void recursiveBubbleSort (int *array, int size) {
             swap(&array[i], &array[i+1]);
             swapped = true;
         }
-    
     if (swapped == false)
         return;
+        
     recursiveBubbleSort(array, size-1);
 }
 
-void iteractiveInsertionSort (int *array, int size) {
+void iterativeInsertionSort (int *array, int size) {
     for (int i = 1; i < size; i++) {
         int aux = array[i];
         int j = i-1;
@@ -58,11 +55,11 @@ void recursiveInsertionSort (int* array, int size) {
     //se for igual a 0, o vetor está vazio, logo não há nada a fazer.
     if(size <= 1)
         return;
+
+    recursiveInsertionSort(array, size-1);
     
     int k = size-2; 
     int aux = array[size-1];
-
-    recursiveInsertionSort(array, size-1);
 
     while(array[k] > aux && k >= 0) {
         array[k+1] = array[k];
@@ -71,7 +68,7 @@ void recursiveInsertionSort (int* array, int size) {
     array[k+1] = aux;
 }
 
-void iteractiveSelectionSort (int *array, int size) {
+void iterativeSelectionSort (int *array, int size) {
     for (int i = 0; i < size; i++) {
         int minor = i;
         for (int j = i+1; j < size; j++) {     
@@ -91,7 +88,8 @@ void recursiveSelectionSort (int* array, int size) {
         if (array[i] > array[major])
             major = i;
     }
-    swap (&array[size-1], &array[major]);
+    if (array[size-1] != array[major])
+        swap (&array[size-1], &array[major]);
 
     recursiveSelectionSort(array, size-1);
 }
@@ -99,10 +97,9 @@ void recursiveSelectionSort (int* array, int size) {
 
 void merge (int *array, int begin, int middle, int end) {
     int iAux, jAux, kAux;
-    int *aux_array = new int[end-begin+1]; //Vetor auxiliar
+    int *aux_array = new int[end-begin+1];
     iAux = begin; jAux = middle+1; kAux = 0;
 
-    //Intercala array[begin..middle] e array[middle+1..end]
     while (iAux <= middle && jAux <= end) {
         if (array[iAux] <= array[jAux])
             aux_array[kAux++] = array[iAux++];
@@ -112,11 +109,20 @@ void merge (int *array, int begin, int middle, int end) {
     while (iAux <= middle) aux_array[kAux++] = array[iAux++];
     while (jAux <= end) aux_array[kAux++] = array[jAux++];
 
-    //Copia vetor ordenado aux para o vetor array
     for (iAux = begin; iAux <= end; iAux++)
         array[iAux] = aux_array[iAux-begin];
 
     delete[] aux_array;
+}
+
+void iterativeMergeSort (int *array, int size) {
+   for (int curr_size = 1; curr_size <= size-1; curr_size *= 2) { 
+       for (int left_start = 0; left_start < size-1; left_start += 2*curr_size) { 
+           int middle = (left_start+curr_size-1 < size-1) ? left_start+curr_size-1 : size-1;
+           int right_end = (left_start+2*curr_size-1 < size-1) ? left_start+2*curr_size-1 : size-1; 
+           merge(array, left_start, middle, right_end); 
+       }
+    }
 }
 
 void recursiveMergeSort (int *array, int begin, int end) {
@@ -125,6 +131,33 @@ void recursiveMergeSort (int *array, int begin, int end) {
         recursiveMergeSort(array, begin, middle);
         recursiveMergeSort(array, middle + 1, end);
         merge(array, begin, middle, end);
+    }
+}
+
+void buildHeap (int *array, int size) {
+    for (int l = 1; l < size; l++)
+        for(int a = l + 1; a > 1 && array[a/2] < array[a]; a = a/2)
+            swap(&array[a/2], &array[a]);
+}
+
+void sieve(int* array, int size){
+    for(int k = 2; k <= size; k = k*2) {
+        if (array[k] < array[k+1] && k < size)
+            k++;
+        // Nesse caso o k é o maior filho da divisão de k/2
+        if (array[k/2] >= array[k])
+            break;
+        swap(&array[k/2], &array[k]);
+    }
+}
+
+void iterativeHeapSort(int* array, int size){
+    buildHeap(array,size);
+    int x = size;
+    while(x >= 2){
+        swap(&array[1], &array[x]);
+        sieve(array, x-1);
+        x--;
     }
 }
 
@@ -141,45 +174,40 @@ int partitionate (int *array, int begin, int end) {
     return i+1;
 }
 
+void iterativeQuickSort(int *array, int begin, int end) { 
+    int stack[end-begin+1];   
+    int top = 0; 
+    stack[top] = begin; 
+    stack[++top] = end; 
+  
+    while (top >= 0) { 
+        end = stack[top--]; 
+        begin = stack[top--]; 
+  
+        int middle = partitionate(array, begin, end); 
+  
+        if (middle-1 > begin) { 
+            stack[++top] = begin; 
+            stack[++top] = middle-1; 
+        } 
+  
+        if (middle+1 < end) { 
+            stack[++top] = middle+1; 
+            stack[++top] = end; 
+        } 
+    } 
+} 
+
 void recursiveQuickSort (int *array, int begin, int end) {
-    if (begin < end) {
+    while (begin < end) {
         int middle = partitionate(array, begin, end);
-        recursiveQuickSort(array, begin, middle-1);
-        recursiveQuickSort(array, middle+1, end);
-    }
-}
-
-
-
- void Biuld_Heap (int *array, int size) {
-    for (int l = 1; l < size; l++) {
-        for(int a = l + 1; a > 1 && array[a/2] < array[a]; a = a/2){
-            swap(&array[a/2], &array[a]);
-            
+        if (middle-begin < end-middle) {
+            recursiveQuickSort(array, begin, middle-1);
+            begin = middle+1;
+        }
+        else {
+            recursiveQuickSort(array, middle+1, end);
+            end = middle-1;
         }
     }
-}
-
-
-void sieve(int* array, int size){
-    for(int k =2; k<=size; k = k*2){
-
-        if (array[k] < array[k+1] && k < size)
-            k++;
-        // Nesse caso o k é o maior filho da divisão de k/2
-        if (array[k/2] >= array[k])
-            break;
-        swap(&array[k/2], &array[k]);
-}
-}
-
-
-void iteractiveHeapSort(int* array, int size){
-   Biuld_Heap(array,size);
-   int x = size;
-   while(x>=2){
-       swap(&array[1], &array[x]);
-       sieve(array, x-1);
-        x--;
-   }
 }
