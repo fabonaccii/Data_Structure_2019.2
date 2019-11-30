65a2d9e3487eb79bc5899f3d1655a055b7cb7d65
Alysson-UFC <51142101+Alysson-UFC@users.noreply.github.com>
2019-11-30 18:43:24 -0300

    Add files via upload

--- Trabalho_3/ordenacaoVetor.h
+++ Trabalho_3/ordenacaoVetor.h
@@ -1,50 +1,50 @@
 #ifndef ORDENACAO_VETOR_H
 #define ORDENACAO_VETOR_H
 
+//struct NoArv;
+
 //Função de troca auxiliar
 void swap (int* x, int* y);
 
+void max_heap(int* array, int size, int index);
+
+void sieve(int* array, int size);
+
+void Biuld_Heap (int *array, int size);
+
 //Bubble Sort iterativo
-void iterativeBubbleSort (int* array, int size);
+void iteractiveBubbleSort (int* array, int size);
 
 //Bubble Sort recursivo
 void recursiveBubbleSort (int* array, int size);
 
 //Insertion Sort iterativo
-void iterativeInsertionSort (int* array, int size);
+void iteractiveInsertionSort (int* array, int size);
 
 //Insertion Sort recursivo
 void recursiveInsertionSort (int* array, int size);
 
 //Selection Sort iterativo
-void iterativeSelectionSort (int* array, int size);
+void iteractiveSelectionSort (int* array, int size);
 
 //Selection Sort recursivo
 void recursiveSelectionSort (int* array, int size);
 
-//Função auxiliar do Merge Sort
-void merge (int *array, int begin, int middle, int end);
 
 //Merge Sort iterativo
-void iterativeMergeSort (int *array, int size);
+void iteractiveMergeSort ();
 
 //Merge Sort recursivo
 void recursiveMergeSort (int *array, int begin, int end);
 
-//Função auxiliar do Heap Sort
-void maxHeap (int * array, int size, int index);
-
 //Heap Sort iterativo
-void iterativeHeapSort (int *array, int size);
+void iteractiveHeapSort(int* array, int size);
 
 //Heap Sort recursivo
-void recursiveHeapSort ();
-
-//Funcão auxiliar do Quick Sort
-int partitionate (int *array, int begin, int end);
+//recursiveHeapSort();
 
 //Quick Sort iterativo
-void iterativeQuickSort (int *array, int begin, int end);
+void iteractiveQuickSort ();
 
 //Quick Sort recursivo
 void recursiveQuickSort (int *array, int begin, int end);
