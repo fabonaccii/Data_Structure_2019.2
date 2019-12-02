20f2505a1d06ea801763e79acc02ffa18bc66219
Fábio Luz <fabioluzduartefilho@alu.ufc.br>
2019-12-01 23:12:16 -0300

    Update ordenacaoLista.h

--- Trabalho_3/ordenacaoLista.h
+++ Trabalho_3/ordenacaoLista.h
@@ -1,8 +1,7 @@
 /* 
 Aluno: Alysson Alexandre de Oliveira Araújo
 Matricula: 474084
-*/
-/* 
+
 Aluno: Fábio Luz Duarte Filho
 Matricula: 474027
 */
@@ -23,18 +22,14 @@ class List {
 
         //Destrutor
         ~List();
+        
+        //Insere um valor na lista
+        void push_back (int key);
 
         //Inserção de elementos de um arquivo para a lista
         void data_file_read_list(int array_size, int *array, const char *data_file_name);
 
-        //Retorna true se a lista estiver vazia ou false, caso contrário
-        bool is_empty();
-
         //Bubble Sort
         void bubbleSort ();
-
-        //Remove todos os elementos da lista e deixa apenas o nó cabeça
-        void clear();
 };
-
 #endif
