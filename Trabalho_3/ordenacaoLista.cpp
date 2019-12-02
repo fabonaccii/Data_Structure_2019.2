187c34da1456611eea54d00e7b535170056c174c
Fábio Luz <fabioluzduartefilho@alu.ufc.br>
2019-12-01 22:50:21 -0300

    Update ordenacaoLista.cpp

--- Trabalho_3/ordenacaoLista.cpp
+++ Trabalho_3/ordenacaoLista.cpp
@@ -23,62 +23,6 @@ List::~List() {
     cout << "Lista destruída" << endl;
 }
 
-
-void List::push_back (int key) {
-        Node *added = new Node;
-        (head->prev)->next = added;
-        added->key = key;
-        added->next = head;
-        added->prev = head->prev;
-        head->prev = added;
-}
-
-int List::pop_back() {
-    if (!is_empty()) {
-        int returnable_value = (head->prev)->key;
-        Node *aux = head->prev;
-        (aux->prev)->next = head;
-        head->prev = aux->prev;
-        delete aux;
-        return returnable_value;
-    }
-    return INT_MIN;
-}
-/*
-void List::insert_after (int key, int index) {
-    if (index >= 0 && index < size()) {
-        Node *aux = head->next;
-        for (int i = 0; i < index; i++)
-            aux = aux->next;
-        Node *added = new Node;
-        added->key = key;
-        added->next = aux->next;
-        added->prev = aux;
-        (aux->next)->prev = added;
-        aux->next = added;
-    }
-}
-*/
-
-void List::print() {
-    Node *aux = head->next;
-    while (aux != head) {
-        cout << aux->key << " ";
-        aux = aux->next;
-    }
-    cout << endl;
-}
-
-int List::size() {
-    int count = 0;
-    Node *aux = head->next;
-    while (aux != head) {
-        count++;
-        aux = aux->next;
-    }
-    return count;
-}
-
 bool List::is_empty() {
     return (head->next == head);
 }
@@ -101,25 +45,31 @@ void List::clear() {
         cerr << "A lista já está vazia" << endl;
 }
 
-
-
-
+void List::data_file_read_list(int array_size, int *array, const char *data_file_name) {	
+	ifstream in_file(data_file_name, ios::binary);
+	
+    	Node* list = new Node;
+    	list = head;
+	
+	for (int i = 0; i < array_size; i++){
+		in_file.read((char*)&array[i], sizeof(int));
+		push_back(array[i]);
+	}
+}
 
 void List::bubbleSort() {
-    
     //Verifica se a lista possui apenas um elemento
     Node* list = head;
-    if(list->next == list && list->prev == list){
+    if(list->next == list && list->prev == list)
         return;
-    }
     /*
     É criado dois node onde o last vai guardar o último elemento e o aux vai guardar o primeiro elemento da lista
     */
     Node* last = list->prev;
     Node* aux = list->next;
-    while(aux!= list){
-        Node* aux2= last;
-        while(aux2!= aux){
+    while(aux != list){
+        Node* aux2 = last;
+        while(aux2 != aux){
             if((aux2->prev)->key > aux2->key){
                 swap((aux2->prev)->key,aux2->key);
             }
@@ -128,83 +78,3 @@ void List::bubbleSort() {
         aux = aux->next;
     }
 }
-
-
-//Não está funcionando
-void List::insertionSort () {
-    //se a lista estiver vazia:
-    Node* list = head;
-    if(list->next== list && list->prev ==list){
-        return;
-    }
-
-    //Se a lista tiver 2 elementos na lista
-    if((list->next)->next == list){
-        if(list->next->key > (list->next)->next->key){
-            swap(list->next->key, (list->next)->next->key);
-            return;
-        }
-    }
-
-
-    //Quando a lista possuir 3 ou mais elementos
-    for(Node* aux = (list->next)->next; aux!= list; aux = aux->next){
-        int Key_aux = aux->key;
-        Node* aux2 = list->next;
-        while(aux2->key >= list->next->key && aux2->key > Key_aux){
-            aux2->next->key = aux2->key;
-            aux2 = aux2->prev;
-        }
-        aux2->next->key = Key_aux;
-        aux2 = aux2->next;
-    }
-    
-} 
-    
-    /*
-    for(Node* aux = list->next; aux!=list; aux = aux->next){
-        Node* aux_prev = list->prev;
-        Node* aux_next = list->next;
-
-        while(aux_next->key < aux_prev->key && aux_prev->next != list){
-
-                
-
-
-        }
-
-    
-    }*/
-
-
-    void listSelectionSort () {
-
-    }
-
-void listMergeSort () {
-
-}
-
-void listHeapSort () {
-
-}
-
-void listQuickSort () {
-
-}
-
-void List::data_file_read_list(int array_size, int *array, const char *data_file_name) {	
-	ifstream in_file(data_file_name, ios::binary);
-	
-    Node* list = new Node;
-    list = head;
-	
-	for (int i = 0; i < array_size; i++){
-		in_file.read((char*)&array[i], sizeof(int));
-		push_back(array[i]);
-	}
-
-}
-
-
-
