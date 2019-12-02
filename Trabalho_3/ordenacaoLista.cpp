6d440669d0a6856f6e92b71c9795b1095b79ee51
Alysson-UFC <51142101+Alysson-UFC@users.noreply.github.com>
2019-12-01 23:04:51 -0300

    Update ordenacaoLista.cpp

--- Trabalho_3/ordenacaoLista.cpp
+++ Trabalho_3/ordenacaoLista.cpp
@@ -1,15 +1,8 @@
-/* 
-Aluno: Alysson Alexandre de Oliveira Araújo
-Matricula: 474084
-*/
-/* 
-Aluno: Fábio Luz Duarte Filho
-Matricula: 474027
-*/
-
 #include <iostream>
 #include "ordenacaoLista.h"
 #include <climits>
+#include <fstream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -31,6 +24,39 @@ List::~List() {
     delete head;
     cout << "Lista destruída" << endl;
 }
+
+
+void List::push_back (int key) {
+        Node *added = new Node;
+        (head->prev)->next = added;
+        added->key = key;
+        added->next = head;
+        added->prev = head->prev;
+        head->prev = added;
+}
+
+int List::pop_back() {
+    if (!is_empty()) {
+        int returnable_value = (head->prev)->key;
+        Node *aux = head->prev;
+        (aux->prev)->next = head;
+        head->prev = aux->prev;
+        delete aux;
+        return returnable_value;
+    }
+    return INT_MIN;
+}
+
+
+void List::print() {
+    Node *aux = head->next;
+    while (aux != head) {
+        cout << aux->key << " ";
+        aux = aux->next;
+    }
+    cout << endl;
+}
+
 
 bool List::is_empty() {
     return (head->next == head);
@@ -54,31 +80,25 @@ void List::clear() {
         cerr << "A lista já está vazia" << endl;
 }
 
-void List::data_file_read_list(int array_size, int *array, const char *data_file_name) {	
-	ifstream in_file(data_file_name, ios::binary);
-	
-    	Node* list = new Node;
-    	list = head;
-	
-	for (int i = 0; i < array_size; i++){
-		in_file.read((char*)&array[i], sizeof(int));
-		push_back(array[i]);
-	}
-}
+
+
+
 
 void List::bubbleSort() {
+    
     //Verifica se a lista possui apenas um elemento
     Node* list = head;
-    if(list->next == list && list->prev == list)
+    if(list->next == list && list->prev == list){
         return;
+    }
     /*
     É criado dois node onde o last vai guardar o último elemento e o aux vai guardar o primeiro elemento da lista
     */
     Node* last = list->prev;
     Node* aux = list->next;
-    while(aux != list){
-        Node* aux2 = last;
-        while(aux2 != aux){
+    while(aux!= list){
+        Node* aux2= last;
+        while(aux2!= aux){
             if((aux2->prev)->key > aux2->key){
                 swap((aux2->prev)->key,aux2->key);
             }
@@ -87,3 +107,37 @@ void List::bubbleSort() {
         aux = aux->next;
     }
 }
+
+
+
+   
+void listSelectionSort () {
+
+}
+
+void listMergeSort () {
+
+}
+
+void listHeapSort () {
+
+}
+
+void listQuickSort () {
+
+}
+
+
+void List::data_file_read_list(int array_size, int *array, const char *data_file_name) {	
+	ifstream in_file(data_file_name, ios::binary);
+	
+    Node* list = new Node;
+    list = head;
+	
+	for (int i = 0; i < array_size; i++){
+		in_file.read((char*)&array[i], sizeof(int));
+		push_back(array[i]);
+	}
+}
+
+
